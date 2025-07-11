c1c7b24ef84ad38fc767eed0c64fbbb0491a4aa2
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-07-11 23:23:12 +0600

    Numbers in python

---
+++ Python/3-numbers.py
@@ -0,0 +1,27 @@
+# There are three numeric types in Python:
+# 1. int
+# 2. float
+# 3. complex    (Complex numbers are written with a "j" as the imaginary)
+
+x = 10
+y = 34.56
+z = 1j + 3
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+a = 56e8
+print(a)
+
+# Type Conversion
+b = float(x)
+c = int(y)
+d = complex(x)
+print(b)
+print(c)
+print(d)
+
+# Random Number
+import random
+print(random.randrange(1, 10))
