d08a7600cb5bdb7f37be67d899ffb7accca5577e
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-07-12 00:01:41 +0600

    Boolean values

---
+++ Python/5-booleans.py
@@ -0,0 +1,19 @@
+# Booleans represent one of two values: True or False.
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+# Truth Values:
+# Almost any value is evaluated to True if it has some sort of content.
+# Any string is True, except empty strings.
+# Any number is True, except 0.
+# Any list, tuple, set, and dictionary are True, except empty ones.
+
+# Falsy Values:
+# bool(False)
+# bool(None)
+# bool(0)
+# bool("")
+# bool(())
+# bool([])
+# bool({})
