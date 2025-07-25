271a076c38701ed4baf7f24223f2564fa653b1bc
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-07-25 23:55:55 +0600

    Sort, copy, join and important methods of list

--- Python/07-list.py
+++ Python/07-list.py
@@ -33,6 +33,7 @@ print(thislist)
 # Add List Items
 thislist.append("orange")
 thislist.extend(mylist)
+
 print(thislist)
 
 # Removing List Items 
@@ -43,3 +44,28 @@ del thislist[0]     # Will remove the indexed item
 # del thislist      # Will remove the list completely
 # thislist.clear()  # Will clear the whole list
 print(thislist)
+
+# Sorting a List
+thislist.sort()     # it'll sort alphabetically
+thislist.sort(reverse=True)     # Sort in descending order
+thislist.sort(key = str.lower)      # Sort in case-insensitive way
+thislist.reverse()      # Reverse the order of list
+
+# Copy List
+newlist1 = thislist.copy()
+newlist2 = list(thislist)
+newlist3 = thislist[:]
+newlist4 = newlist1 + newlist2
+
+# Other methods of list
+# append()  -->	    Adds an element at the end of the list
+# clear()   -->	    Removes all the elements from the list
+# copy()    -->	    Returns a copy of the list
+# count()   -->	    Returns the number of elements with the specified value
+# extend()  -->	    Add the elements of a list (or any iterable), to the end of the current list
+# index()   -->	    Returns the index of the first element with the specified value
+# insert()  -->	    Adds an element at the specified position
+# pop()     -->	    Removes the element at the specified position
+# remove()  -->	    Removes the item with the specified value
+# reverse() -->	    Reverses the order of the list
+# sort()    -->	    Sorts the list
