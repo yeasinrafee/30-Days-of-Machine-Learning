3bbabb26405dbce0657d3e35183a7a24ce18873f
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-07-25 23:29:19 +0600

    Creating list, accessing and inserting list items

---
+++ Python/07-list.py
@@ -0,0 +1,31 @@
+# Lists are used to store multiple items in a single variable.
+
+mylist = ["Yeasin", "Arefin", "Rafee"]
+print(mylist)
+
+# Rules:
+#  1. List items are ordered, changeable, and allow duplicate values.
+#  2. Ordered means that the items have a defined order, and that order will not change.
+#  3. Adding new items to a list, the new items will be placed at the end of the list.
+#  4. The list is changeable, meaning that we can change, add, and remove items in a list after it has been created.
+
+print(len(mylist))
+print(type(mylist))
+
+
+# It is also possible to use the list() constructor when creating a new list.
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+# Accessing list items
+print(thislist[1])      # // Access From First
+print(thislist[-1])     # // Access From Last
+print(thislist[2:5])    # // Return List from middle
+print(thislist[:5])    # // Return List from first to given number
+print(thislist[2:])    # // Return List from given number to last
+
+# Inserting List Item
+thislist[1] = "pineapple"
+thislist[2:3] = ["watermelon"]
+thislist.insert(1, "cherry")
+print(thislist) 
