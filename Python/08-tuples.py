0a54f1d229769cb14334fd57faed6ced08333c40
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-08-01 13:30:36 +0600

    Tuple initialization and accessing

---
+++ Python/08-tuples.py
@@ -0,0 +1,18 @@
+mytuple = ("Apple", "Banana", "cherry")
+
+print(mytuple)
+print(len(mytuple))
+print(type(mytuple))
+
+tuple1 = ("abc", 34, True, 90, "male")
+thistuple = tuple(("apple", "banana", "cherry"))
+print(thistuple)
+print(tuple1)
+
+# Accessing a Tuple: 
+print(thistuple[1])
+print(thistuple[-1])
+print(tuple1[1:4])
+print(thistuple[:1])
+print(thistuple[1:])
+print(tuple1[-4:-1])
