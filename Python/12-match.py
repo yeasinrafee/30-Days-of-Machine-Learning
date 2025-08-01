634951b45df08ddf1dc2d48cd74ea479a54abbad
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-08-01 23:46:59 +0600

    Match in python

---
+++ Python/12-match.py
@@ -0,0 +1,18 @@
+day = 1
+match day:
+    case 1:
+        print("Saturday")
+    case 2:
+        print("Sunday")
+    case 3:
+        print("Monday")
+    case 4:
+        print("Tuesday")
+    case 5:
+        print("Wednesday")
+    case 6:
+        print("Thursday")
+    case 7:
+        print("Friday")
+    case _:
+        print("Invalid day")
