62b2d33f0606c2edee5f5f97a0d162b7f700be9a
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-08-02 12:59:06 +0600

    Python user input

---
+++ Python/25-user-input.py
@@ -0,0 +1,13 @@
+print("Enter your name: ")
+name = input()
+print(f"Hello {name}")
+
+# Using Prompt
+fname = input("Enter your first name: ")
+print(f"Hello {fname}")
+
+# Input number
+x = input ("Enter a number: ")
+print(type(x))
+y = float(x)
+print(type(y))
