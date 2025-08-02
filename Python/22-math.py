87ea28e84dd084946610b1dd020297a8460ccd16
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-08-02 12:32:13 +0600

    Math in python

---
+++ Python/22-math.py
@@ -0,0 +1,24 @@
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+a = abs(-7.25)
+print(a)
+
+x = pow(4, 3)
+print(x)
+
+b = math.sqrt(64)
+print(b)
+
+c = math.ceil(1.4)
+d = math.floor(1.4)
+print(c)
+print(d)
+
+p = math.pi
+print(p)
